47b6cd0ceb32500c9cb93d2b60e8124c17f2569b
Oliver Whitehead <oliver@synx.games>
2023-02-11 23:46:55 +0000

    feat(sort): Implement basic sort objects
    Implements a basic system to map int : sort. The sorts are not yet implemented but provides the needed backbone for the `Pack` object.

---
+++ CMP1903M A01 2223/Sort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CMP1903M_A01_2223 {
+    interface Sort {
+        Pack Sort(Pack pack);
+    }
+
+    class NoShuffleSort : Sort {
+        public Pack Sort(Pack pack) {
+            return pack;
+        }
+    }
+
+    class FisherYatesShuffleSort : Sort {
+        public Pack Sort(Pack pack) {
+            throw NotImplementedException();
+        }
+    }
+
+    class RiffleShuffleSort : Sort {
+        public Pack Sort(Pack pack) {
+            throw NotImplementedException();
+        }
+    }
+
+    class Sorts {
+        public static readonly Sort NO_SHUFFLE = new NoShuffleSort();
+        public static readonly Sort FISHER_YATES_SHUFFLE = new FisherYatesShuffleSort();
+        public static readonly Sort RIFFLE_SHUFFLE = new RiffleShuffleSort();
+
+        public static Sort GetSort(int sortId) {
+            return GetSortOrDefault(sortId, NO_SHUFFLE);
+        }
+        
+        public static Sort GetSortOrDefault(int sortId, Sort defaultSort) {
+            switch (sortId) {
+                case 1:
+                    return FISHER_YATES_SHUFFLE;
+                case 2:
+                    return RIFFLE_SHUFFLE;
+                case 3:
+                    return NO_SHUFFLE;
+                default:
+                    return defaultSort;
+            }
+        }
+    }
+}
