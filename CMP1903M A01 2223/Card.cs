2e65ee66cdd3b1696b1e75fe47596477f8107088
Oliver Whitehead <oliver@synx.games>
2023-02-12 00:29:33 +0000

    style(project): Fix all remaining styling issues

--- CMP1903M A01 2223/Card.cs
+++ CMP1903M A01 2223/Card.cs
@@ -6,18 +6,17 @@ using System.Threading.Tasks;
 
 namespace CMP1903M_A01_2223 {
     class Card {
-
         private readonly SuitType suitType;
         private readonly int value;
-        
+
         public Card(SuitType suitType, int value) {
             this.suitType = suitType;
 
             if (value <= 0 || value > 13) {
                 throw new ArgumentOutOfRangeException(nameof(value), "Card value must be > 0 and < 13!");
             }
+
             this.value = value;
         }
-        
     }
 }
--- CMP1903M A01 2223/Util.cs
+++ CMP1903M A01 2223/Util.cs
@@ -3,8 +3,6 @@ using System.Threading;
 
 namespace CMP1903M_A01_2223 {
     public class Util {
-        
         public static readonly Random RANDOM = new Random();
-
     }
 }
